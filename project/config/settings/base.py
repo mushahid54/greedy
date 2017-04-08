20d6e9d07f5296cef2f0cc34dd6c7e412b2414bf
Mushahid <mushahid.khan@flyrobe.com>
2017-04-08 18:06:33 +0530

    update the changes

--- project/config/settings/base.py
+++ project/config/settings/base.py
@@ -126,4 +126,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+#
